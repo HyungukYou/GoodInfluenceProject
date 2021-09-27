b977161ebba7feaf7e0e59950b5acd3df5140729
hyunguk <hyunguk119@gmail.com>
2021-09-28 03:08:41 +0900

    Change the name.

--- src/main/java/com/example/hyungukservice/controller/HyunGukController.java
+++ src/main/java/com/example/hyungukservice/controller/HyunGukController.java
@@ -17,37 +17,37 @@ public class HyunGukController {
     @Autowired
     private HyunGukService hyunGukService;
 
-    @GetMapping("text")
+    @GetMapping("getShop") // 모든 가게 정보 가져오기
     @ResponseBody
     public List<HyunGuk> getShop(){
         return hyunGukService.getShop();
     }
-    @GetMapping("text1")
+    @GetMapping("getShopIdByShopName") // 가게이름을 주면 해당 id를 준다
     @ResponseBody
     public Long getShopIdByShopName(String shop) throws IllegalAccessException {
        return hyunGukService.getShopIdByShopName(shop);
     }
-    @DeleteMapping("text2")
+    @DeleteMapping("AllDeleteShop") // 모든 가게 정보를 삭제한다
     @ResponseBody
     public void AllDeleteShop(){
         hyunGukService.AllDeleteShop();
     }
-    @DeleteMapping("text3")
+    @DeleteMapping("DeleteShopById") // id를 주면 id에 해당하는 가게 정보를 전부 삭제
     @ResponseBody
     public void DeleteShopById(Long id){
         hyunGukService.DeleteShopById(id);
     }
-    @GetMapping("text4")
+    @GetMapping("CheckShop") // 가게가 있는지 없는지 확인
     @ResponseBody
     public boolean CheckShop(String shop){
         return hyunGukService.CheckShop(shop);
     }
-    @PostMapping("text5")
+    @PostMapping("CreateShopImFormationCheckDuplication") // 중복 가게가 있는지 확인하고 있으면 에러 없으면 생성
     @ResponseBody
     public HyunGuk CreateShopImFormationCheckDuplication(String area, String shop, String address, String phone, String sponsor) throws IllegalAccessException {
         return hyunGukService.CreateShopImFormationCheckDuplication(area,shop,address,phone,sponsor);
     }
-    @GetMapping("text6")
+    @GetMapping("getShopName") // 가게 이름을 주면 그 가게 해당하는 정보를 가져온
     @ResponseBody
     public String getShopName(String shop) throws IllegalAccessException {
         return hyunGukService.getShopName(shop);
